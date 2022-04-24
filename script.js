40441fb99c1adfb7a114bd03060a0097fe8f9ce4
Quang Duy <nguyenlequangduy998@gmail.com>
2022-04-24 13:41:01 +0700

    Add press enter key event

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let button = document.getElementById("btn");
+let text = document.getElementById("txt");
 
 button.onclick = () => {
-    let text = document.getElementById("txt");
     let congratsMessage = document.getElementById("message");
     congratsMessage.innerHTML = "Happy birthday to " + text.value + "!";
 }
 
-
+document.body.addEventListener("keyup", (event) => {
+    if (event.key == "Enter") {
+        event.preventDefault();
+        button.click();
+    }
+});
